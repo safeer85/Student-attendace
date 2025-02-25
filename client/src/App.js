eff2c80b2158746cd59d56d1975cca28cee43e35
safeer85 <msafeer753@gmail.com>
2025-02-25 10:33:08 +0530

    again1

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import LoginPage from './pages/Login/LoginPage.tsx';
 import {  Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/Register/RegisterPage.tsx';
-import HomePage from './pages/Home/home.tsx';
+//import HomePage from './pages/Home/home.tsx';
 
 
 function App() {

4e65c7752e235583805ba31d11b22f5d388cbc87
safeer85 <msafeer753@gmail.com>
2025-02-25 10:05:50 +0530

    again changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
+       {/* <Route path="/home" element={<HomePage />} />*/}
         
       </Routes>
   );

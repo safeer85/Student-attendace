c06031b9c585c68f6c8c064a3be485d73b0f86bd
safeer85 <msafeer753@gmail.com>
2025-02-25 10:08:04 +0530

    again

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -92,9 +92,9 @@ export default HomePage;*/
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 //import Navbar from '../../Components/navbar.tsx'; // Import the Navbar component
-import TakeAttendance from '../../Components/TakeAttendance.tsx'; // Import the Take Attendance component
-import ViewAttendance from '../../Components/ViewAttendance.tsx';
-import GenerateReport from '../../Components/GenerateReport.tsx';
+//import TakeAttendance from '../../Components/TakeAttendance.tsx'; // Import the Take Attendance component
+//import ViewAttendance from '../../Components/ViewAttendance.tsx';
+//import GenerateReport from '../../Components/GenerateReport.tsx';
 
 const HomePage: React.FC = () => {
   const [activeComponent, setActiveComponent] = useState<string | null>(null);
@@ -133,19 +133,19 @@ const HomePage: React.FC = () => {
             {activeComponent === 'viewAttendance' && (
               <div className="mt-4">
                 <h4 className="text-xl font-semibold">View Students Attendance Details</h4>
-                <ViewAttendance />
+                
               </div>
             )}
             {activeComponent === 'takeAttendance' && (
               <div className="mt-4">
                 <h4 className="text-xl font-semibold">Take Attendance</h4>
-                <TakeAttendance />
+                
               </div>
             )}
             {activeComponent === 'generateReport' && (
               <div className="mt-4">
                 <h4 className="text-xl font-semibold">Generate Report</h4>
-                <GenerateReport />
+                
               </div>
             )}
           </div>
@@ -160,7 +160,7 @@ const HomePage: React.FC = () => {
             {activeComponent === 'viewAttendance' && (
               <div className="mt-4">
                 <h4 className="text-xl font-semibold">Your Attendance Details</h4>
-                <ViewAttendance />
+                
               </div>
             )}
           </div>
@@ -175,7 +175,7 @@ const HomePage: React.FC = () => {
             {activeComponent === 'viewAttendance' && (
               <div className="mt-4">
                 <h4 className="text-xl font-semibold">Child’s Attendance Details</h4>
-                <ViewAttendance />
+                
               </div>
             )}
           </div>

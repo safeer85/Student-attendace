f5e8c9a44a103f3068ede34be16c2ca5455874d8
safeer85 <msafeer753@gmail.com>
2025-02-25 14:50:32 +0530

    remove verddion

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import LoginPage from './pages/Login/LoginPage.tsx';
 import {  Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/Register/RegisterPage.tsx';
-//import HomePage from './pages/Home/home.tsx';
+import HomePage from './pages/Home/home.tsx';
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-       {/* <Route path="/home" element={<HomePage />} />*/}
+        <Route path="/home" element={<HomePage />} />
         
       </Routes>
   );
